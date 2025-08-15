98d2f9cc43180b7f9b36b56539068a1ffb08105e
Atindekoun israel <114492317+atindekounisrael@users.noreply.github.com>
2025-08-15 17:45:20 +0000

    Mise à jour de main.jsx pour inclure la route BlogPost

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Blog from './pages/Blog';
-import BlogPost from './pages/BlogPost.jsx';
+import BlogPost from './pages/BlogPost';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/blog" element={<Blog />} />
       <Route path="/blog/:slug" element={<BlogPost />} />
-      {/* Autres routes */}
+      {/* Ajoutez ici les autres routes existantes */}
     </Routes>
   </BrowserRouter>
 );
